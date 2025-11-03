600a709cb23e40764d3f3649e2d4807ee9f1bc29
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-11-03 14:19:35 +0100

    fibonacci

--- src/TDD/fibonacci/Fibonacci.cs
+++ src/TDD/fibonacci/Fibonacci.cs
@@ -1,10 +1,59 @@
 namespace TDD.Fibonacci
 {
-    public class Fibonacci
+    public interface IFibonacci
     {
-        public int Calc(int index)
+        public int Calculate(int index);
+    }
+
+    public abstract class FibonacciBaseImpl : IFibonacci
+    {
+        public int Calculate(int index)
         {
-            return 0;
+            // validation
+            if (index < 0) {
+                throw new ArgumentException("Index must be >= 0");
+            }
+            if (index > 46) {
+                throw new ArgumentException("Index must be <= 46");
+            }
+
+            return CalculateImpl(index);
+        }
+
+        protected abstract int CalculateImpl(int index);
+    }
+
+    public class FibonacciLoopImpl : FibonacciBaseImpl
+    {
+        protected override int CalculateImpl(int index)
+        {
+            if (index < 2) {
+                return index;
+            }
+
+            int lastBut1 = 0;
+            int last = 1;
+            int result = 0;
+
+            for (int i = 2; i <= index; i++)
+            {
+                result = last + lastBut1;
+                lastBut1 = last;
+                last = result;
+            }
+
+            return result;
+        }
+    }
+
+    public class FibonacciRecursiveImpl : FibonacciBaseImpl
+    {
+        protected override int CalculateImpl(int index)
+        {
+            if (index < 2) {
+                return index;
+            }
+            return CalculateImpl(index - 1) + CalculateImpl(index - 2);
         }
     }
 }
--- tests/TDD.Tests/fibonacci/FibonacciTest.cs
+++ tests/TDD.Tests/fibonacci/FibonacciTest.cs
@@ -1,71 +1,62 @@
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using TDD.Fibonacci;
+
+// https://www.wackerart.de/mathematik/big_numbers/fibonacci_numbers.html
 
 namespace TDD.Tests.Fibonacci
 {
-    public class FibonacciTest
-    {
+  public abstract class FibonacciTest {
+    protected FibonacciTest(IFibonacci fibonacci) {
+      this.fibonacci = fibonacci;
+    }
 
-        [SetUp]
-        public void beforeEach()
-        {
-        }
+    public IFibonacci fibonacci;
 
-        [Test]
-        public void ShouldYield0For0()
-        {
-            // Arrange  ("given")
-            TDD.Fibonacci.Fibonacci fibonacci = new TDD.Fibonacci.Fibonacci();
-            int index = 0;
-
-            // Act      ("when")
-            int actual = fibonacci.Calc(index);
-
-            // Assert   ("then")
-            Assert.That(actual, Is.EqualTo(0));
-        }
-
-
-        [Test]
-        [Ignore("ToDo")]
-        public void ShouldFailForMinus1()
-        {
-        }
-
-
-        [Test]
-        [Ignore("ToDo")]
-        public void ShouldFailFor47()
-        {
-        }
-
-    /*[TestCase(0, 0)]
+    [TestCase(0, 0)]
     [TestCase(1, 1)]
     [TestCase(2, 1)]
     [TestCase(3, 2)]
+    [TestCase(4, 3)]
     [TestCase(5, 5)]
     [TestCase(6, 8)]
-    [TestCase(7, 13)]
-    [TestCase(19, 4_181)]
-    [TestCase(30, 832_040)]
-    public void shouldYieldFibonacci(int index, int expected)
-    {
-      // Act (when)
+    public void Calc_shouldPassForSmallIndices(int index, int expected) {
       int actual = fibonacci.Calculate(index);
-
-      // Assert (then)
       Assert.That(actual, Is.EqualTo(expected));
-    }*/
+    }
 
-    /*[TestCase(-1, "Index must not be negative")]
-    [TestCase(47, "Indext must not be greater than 46")]
-    public void shouldThrowError(int index, string expected)
-    {
-      // Act (when)
-      ArgumentException e = Assert.Throws<ArgumentException>(() => fibonacci.Calculate(index));
+    [TestCase(19, 4_181)]
+    [TestCase(27, 196_418)]
+    public void Calc_shouldPassForMediumIndices(int index, int expected) {
+      int actual = fibonacci.Calculate(index);
+      Assert.That(actual, Is.EqualTo(expected));
+    }
 
-      // Assert (then)
+
+    [TestCase(-1, "Index must be >= 0")]
+    [TestCase(47, "Index must be <= 46")]
+    public void Calc_shouldFail(int index, string expected) {
+      ArgumentException e = Assert.Throws<ArgumentException>(() => fibonacci.Calculate(index));  
       Assert.That(e.Message, Is.EqualTo(expected));
-    }*/
+    }
   }
+
+  public class FibonacciLoopImplTest : FibonacciTest {
+    public FibonacciLoopImplTest() : base(new FibonacciLoopImpl()) 
+    {}
+
+    [TestCase(45, 1_134_903_170)]
+    [TestCase(46, 1_836_311_903)]
+    public void Calc_shouldPassForLargeIndices(int index, int expected) {
+      int actual = fibonacci.Calculate(index);
+      Assert.That(actual, Is.EqualTo(expected));
+    }
+  }
+
+  public class FibonacciRecursiveImplTest : FibonacciTest {
+    public FibonacciRecursiveImplTest() : base(new FibonacciRecursiveImpl()) 
+    {}
+  }
+
+
 }

90c07e10184e1bc44251078f5036d50e8fd2faae
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-11-03 10:33:06 +0100

    hello, Strings

--- src/TDD/Hello.cs
+++ src/TDD/Hello.cs
@@ -2,29 +2,9 @@ namespace TDD
 {
     public static class Hello
     {
-        public static string GetHello()
-        {
-            return "friends";
-        }
-
         public static int Answer(string question)
         {
-            return 43;
-        }
-
-        public static List<string> GetList()
-        {
-            return new List<string>
-            {
-                "a",
-                "b",
-                "c"
-            };
-        }
-
-        public static (string FirstName, string LastName) GetObject()
-        {
-            return ("Joey", "Ramone");
+            return 42;
         }
     }
 }
--- tests/TDD.Tests/matchers/StringsTest.cs
+++ tests/TDD.Tests/matchers/StringsTest.cs
@@ -7,45 +7,55 @@ namespace TDD.Tests.Matchers
     public class StringsTest
     {
         [Test]
-        [Ignore("TODO: Verify that `MyEmail` is a string")]
         public void MyEmailIsString()
         {
+            Assert.That(Strings.MyEmail, Is.TypeOf<string>(), "MyEmail should be of type string");
         }
 
         [Test]
-        [Ignore("TODO")]
         public void MyEmailStartsWithAndreas()
         {
+            Assert.That(Strings.MyEmail, Does.StartWith("andreas"), "MyEmail should start with 'andreas'");
         }
 
         [Test]
-        [Ignore("TODO")]
         public void MyEmailEndsWithDotEu()
         {
+            Assert.That(Strings.MyEmail, Does.EndWith(".eu"), "MyEmail should end with '.eu'");
         }
 
         [Test]
-        [Ignore("TODO")]
         public void MyEmailDoesNotEndWithDotCom()
         {
+            Assert.That(Strings.MyEmail, Does.Not.EndWith(".com"), "MyEmail should not end with '.com'");
         }
 
         [Test]
-        [Ignore("TODO")]
         public void MyEmailIncludesBinary()
         {
+            Assert.That(Strings.MyEmail, Does.Contain("binary"), "MyEmail should contain 'binary'");
         }
 
         [Test]
-        [Ignore("TODO")]
         public void MyEmailIncludesAndreasAndStars()
         {
+            Assert.That(Strings.MyEmail, Does.Contain("andreas").And.Contain("stars"), "MyEmail should contain both 'andreas' and 'stars'");    
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(Strings.MyEmail, Does.Contain("andreas"), "MyEmail should contain 'andreas'");
+                Assert.That(Strings.MyEmail, Does.Contain("stars"), "MyEmail should contain 'stars'");
+            });
         }
 
         [Test]
-        [Ignore("TODO")]
+        [Description("Should verify that MyEmail matches a basic email regex pattern")]
         public void MyEmailMatchesRegex()
         {
+            String pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Assert.That(
+                Strings.MyEmail, Does.Match(pattern),
+                "MyEmail should match the email regex pattern");  
         }
     }
 }

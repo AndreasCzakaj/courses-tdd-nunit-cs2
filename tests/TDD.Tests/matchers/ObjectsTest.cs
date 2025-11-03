487b33cb0ed1c8e44010ceb2ef4df5f6b5065780
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-11-03 13:47:03 +0100

    objects

--- tests/TDD.Tests/matchers/ObjectsTest.cs
+++ tests/TDD.Tests/matchers/ObjectsTest.cs
@@ -17,13 +17,23 @@ namespace TDD.Tests.Matchers
         }
 
         [Test]
-        [Description("Should verify that the people list is initialized and has 1000 entries")]
-        public void Init_ShouldHaveCorrectSize()
+        [Description("Should verify that person has FirstName 'Kim'")]
+        public void VerifyPersonHasFirstNameKim()
         {
-            Assert.That(people, Has.Count.EqualTo(1000), "People list should have 1000 entries");
+            Assert.That(person.FirstName, Is.EqualTo("Kim"));
         }
 
         [Test]
+        public void VerifyPersonContainsFields()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(person, Has.Property("FirstName").And.Property("Email"));
+            });
+        }
+
+        [Test]
+        [Description("Should verify that person is equal to expected person but not same")]
         public void VerifyPersonEqualsPerson()
         {
             var expectedPerson = new Person
@@ -35,32 +45,50 @@ namespace TDD.Tests.Matchers
                 IpAddress = "55.247.214.105",
             };
 
-            // Compare objects by properties (NUnit 4.4+)
             Assert.That(person, Is.EqualTo(expectedPerson).UsingPropertiesComparer());
 
-            // Compare objects by properties (NUnit 4.4+)
-            Assert.That(person, Is.EqualTo(expectedPerson).UsingPropertiesComparer());
-
-            // Verify they are not the same object reference
             Assert.That(person, Is.Not.SameAs(expectedPerson));
         }
 
         [Test]
-        [Ignore("TODO: Should verify that person has FirstName 'Kim'")]
-        public void VerifyPersonHasFirstNameKim()
+        [Description("Should verify that person is almost equal to expected person exept for Id")]
+        public void VerifyPersonEqualsPersonExceptId()
         {
+            var expectedPerson = new Person
+            {
+                Id = 340643069,
+                FirstName = "Kim",
+                LastName = "Rawcliffe",
+                Email = "krawcliffen@seesaa.net",
+                IpAddress = "55.247.214.105",
+            };
+
+            Assert.That(person, Is.EqualTo(expectedPerson).UsingPropertiesComparer(
+                o => o.Excluding(nameof(Person.Id))
+            ));
         }
 
         [Test]
-        [Ignore("TODO: Should verify that person has fields 'FirstName' and 'Email'")]
-        public void VerifyPersonContainsFields()
-        {
-        }
-
-        [Test]
-        [Ignore("TODO: Should verify that person contains values LastName: 'Rawcliffe' and Email: 'krawcliffen@seesaa.net'")]
+        [Description("Should verify that person contains values LastName: 'Rawcliffe' and Email: 'krawcliffen@seesaa.net'")]
         public void VerifyPersonContainsValues()
         {
+            var expectedPerson = new Person
+            {
+                LastName = "Rawcliffe",
+                Email = "krawcliffen@seesaa.net",
+            };
+            
+            // variant a
+            Assert.That(person, Is.EqualTo(expectedPerson).UsingPropertiesComparer(
+                o => o.Using(nameof(Person.LastName), nameof(Person.Email)
+            )));
+            
+            // variant b
+            Assert.That(
+                person, 
+                Has.Property("LastName").EqualTo("Rawcliffe").
+                And.Property("Email").EqualTo("krawcliffen@seesaa.net")
+            );
         }
     }
 }
